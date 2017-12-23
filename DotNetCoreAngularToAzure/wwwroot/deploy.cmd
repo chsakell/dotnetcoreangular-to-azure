82fde7c7eb9e4e1ee1d41ed588269d52a6a66f01
chsakell <chsakell@gmail.com>
2017-12-24 00:09:44 +0200

    add npm-install powershell script

--- DotNetCoreAngularToAzure/wwwroot/deploy.cmd
+++
@@ -1,9 +0,0 @@
-:: 6. Install npm packages at DEPLOYMENT_TARGET
-echo =======  Installing npm packages: Starting at %TIME% ======= 
-IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
-  pushd "%DEPLOYMENT_TARGET%"
-  call :ExecuteCmd !NPM_CMD! install --production
-  IF !ERRORLEVEL! NEQ 0 goto error
-  popd
-)
-echo =======  Installing npm packages: Finished at %TIME% =======
